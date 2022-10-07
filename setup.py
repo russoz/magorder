36a26327a7d638cebaa214536e248a08a3e7c032
Alexei Znamensky <russoz@gmail.com>
2022-10-07 23:16:53 +1300

    bump version to 0.4 - manually, no tags

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
 setup(name='magorder',
-      version='0.1',
+      version='0.4',
       packages=find_packages(),
       )
